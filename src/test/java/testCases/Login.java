4e396d69f9f8e7b4d7b905394840caad793b02a2
mansur <mansurk184@gmail.com>
2022-09-23 14:07:15 +0545

    negative scenarios for login

--- src/test/java/testCases/Login.java
+++ src/test/java/testCases/Login.java
@@ -1,16 +1,9 @@
 package testCases;
 
-import java.util.concurrent.TimeUnit;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.BeforeTest;
 import org.testng.Assert;
 import org.testng.annotations.*;
-
-import io.github.bonigarcia.wdm.WebDriverManager;
-
 import POM.LoginPom;
 import POM.Configuration;
 
@@ -18,10 +11,15 @@ public class Login {
 	LoginPom pom;
 	public WebDriver driver;
 
-	@BeforeTest
+	@BeforeMethod
 	public void setup() {
 		driver = Configuration.browser();
 		driver.get(Configuration.WebUrl());
+		
+	}
+	@AfterMethod
+	public void end() {
+		driver.close();
 	}
 
 	@Test(priority = 1, description = "This is test for successfull Login")
@@ -32,5 +30,44 @@ public class Login {
 		Assert.assertTrue(pom.getUsername().contains("Vsoft"));
 
 	}
+	
+	@Test(priority=2,description="This is test for invalid email")
+	public void invalidEmail() {
+		pom = new LoginPom(driver);
+		pom.clickSignIn();
+		pom.LoginToWeb("mansurhhh@test.com", "PKR@PKR");
+		String expected="Authentication failed.";
+		String actual=driver.findElement(By.xpath("//li[contains(text(),'Authentication failed.')]")).getText();
+		Assert.assertEquals(actual, expected);
+		
+	}
+	@Test(priority=3,description="This is test for invalid password")
+	public void invalidPassword() {
+		pom = new LoginPom(driver);
+		pom.clickSignIn();
+		pom.LoginToWeb("mansur@test.com", "PKR@PKRasg");
+		String expected="Authentication failed.";
+		String actual=driver.findElement(By.xpath("//li[contains(text(),'Authentication failed.')]")).getText();
+		Assert.assertEquals(actual, expected);
+	}
+	@Test(priority=4,description="This is test for empty email")
+	public void emptyEmail() {
+		pom = new LoginPom(driver);
+		pom.clickSignIn();
+		pom.LoginToWeb(" ", "PKR@PKR");
+		String expected="An email address required.";
+		String actual=driver.findElement(By.xpath("//li[contains(text(),'An email address required.')]")).getText();
+		Assert.assertEquals(actual, expected);
+	}
+	@Test(priority=5,description="This is test for empty password")
+	public void emptyPassword() {
+		pom = new LoginPom(driver);
+		pom.clickSignIn();
+		pom.LoginToWeb("mansur@test.com", " ");
+		String expected="Password is required.";
+		String actual=driver.findElement(By.xpath("//li[contains(text(),'Password is required.')]")).getText();
+		Assert.assertEquals(actual, expected);
+	}
+
 
 }
